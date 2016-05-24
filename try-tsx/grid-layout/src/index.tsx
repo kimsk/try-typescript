dd827d4a3647eea0e30f04cc5591d596321b7721
Karlkim Suwamongkol <contact@karlk.im>
2016-05-24 09:04:47 -0700

    use react-grid-layout

--- try-tsx/grid-layout/src/index.tsx
+++ try-tsx/grid-layout/src/index.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+var ReactGridLayout = require('react-grid-layout');
 
-import { Hello } from "./components/Hello";
-
+var MyFirstGrid = React.createClass({
+  render: function() {
+    // layout is an array of objects, see the demo for more complete usage
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+    return (
+      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key={'a'}>a</div>
+        <div key={'b'}>b</div>
+        <div key={'c'}>c</div>
+      </ReactGridLayout>
+    )
+  }
+});
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <MyFirstGrid/>,document.getElementById("example")
 );
