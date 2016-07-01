d7c5e905347738e73238f3ef906847f3b4c4a06e
Karlkim Suwamongkol <contact@karlk.im>
2016-06-30 18:49:20 -0700

    multiple modules

--- try-tsx/proj/src/index.tsx
+++ try-tsx/proj/src/index.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import ABC = require('./modules');
+
+ABC.A.a();
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
---
+++ try-tsx/proj/src/modules.ts
@@ -0,0 +1,29 @@
+export module A {
+  export function a(){
+    console.log('A.a');
+  }
+
+  export function b(){
+    console.log('A.b');
+  }
+}
+
+export module B {
+  export function a(){
+    console.log('B.a');
+  }
+
+  export function b(){
+    console.log('B.b');
+  }
+}
+
+export module C {
+  export function a(){
+    console.log('C.a');
+  }
+
+  export function b(){
+    console.log('C.b');
+  }
+}
