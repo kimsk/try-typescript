177504fe1feb4ccdcc93c4a9bd01dbcef575d6ea
Karlkim Suwamongkol <contact@karlk.im>
2017-01-07 20:32:44 -0800

    setIn

--- ts-immutable-react/src/components/Main.tsx
+++ ts-immutable-react/src/components/Main.tsx
@@ -5,29 +5,50 @@ import { A } from './A'
 import { B } from './B'
 import { C } from './C'
 
+interface Props {
+  obj: any
+}
+
 const aProps = {
   obj: Immutable.fromJS({ text: 'Hello', text2: 'A' })
 }
 
-const cProps = aProps
-cProps.obj = aProps.obj.setIn(['text2'], () => 'C')
 
 interface MainState {
-  count: number
+  count?: number
+  cProps?: Props
 }
 
 export default class Main extends React.Component<{}, MainState>{
   constructor(){
     super()
-    this.state = { count: 0 }
+    this.state = {
+      count: 0,
+      cProps: { obj: aProps.obj.setIn(['text2'], 'C') }
+    }
+  }
+
+  buttonPress = () => {
+    let count = this.state.count + 1
+    let cProps = this.state.cProps
+
+    if(count % 2 === 0){
+      cProps.obj = cProps.obj.setIn(['text2'], `C-${count}`)
+    }
+
+    this.setState({
+      count,
+      cProps
+    })
   }
 
   render(){
+    let { count, cProps } = this.state
     return (<div>
-      <button onClick={() => this.setState({ count: this.state.count + 1 })}>Press</button>
-      <div>Test {this.state.count}</div>
+      <button onClick={this.buttonPress}>Press</button>
+      <div>Test {count}</div>
       <A { ...aProps }/>
-      <B text={`World ${this.state.count}`}/>
+      <B text={`World ${count}`}/>
       <C { ...cProps }/>
     </div>)
   }
